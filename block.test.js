d5b3552a489eea2c14e3448e26599e4cc1726ec7
gwen1985 <gwen.ellegeest@outlook.com>
2021-02-15 01:54:32 +0100

    added a mineBlock() function, inspecting the lasthash from previous block + test (passed).

--- block.test.js
+++ block.test.js
@@ -29,4 +29,26 @@ describe('Block', () => {
         });
     });
 
+    describe('mineBlock()', () => {
+        const lastBlock = Block.genesis();
+        const data = 'mined data';
+        const minedBlock = Block.mineBlock({lastBlock, data});
+
+        it('should return a Block instance',  () => {
+            expect(minedBlock instanceof Block).toBe(true);
+        });
+
+        it('sets the `lastHash` to be the `hash` of the last block', () => {
+            expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+        });
+
+        it('sets the `data`', () => {
+            expect(minedBlock.data).toEqual(data);
+        })
+
+        it('sets the `timestamp`', () => {
+            expect(minedBlock.timestamp).not.toEqual(undefined);
+        });
+    });
+
 });

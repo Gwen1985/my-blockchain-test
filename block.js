3c6c137edd8ffe8a09ca523fcba173633dd8a5da
gwen1985 <gwen.ellegeest@outlook.com>
2021-02-15 15:50:48 +0100

    added crypto SHA-256 functionality

--- block.js
+++ block.js
@@ -1,4 +1,6 @@
 const {GENESIS_DATA} = require('./config');
+const cryptoHash = require('./crypto-hash');
+
 
 class Block {
 
@@ -14,10 +16,14 @@ class Block {
     }
 
     static mineBlock ({lastBlock, data}) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+
         return new this({
-            timestamp: Date.now(),
-            lastHash: lastBlock.hash,
-            data
+            timestamp,
+            lastHash,
+            data,
+            hash: cryptoHash(timestamp, lastHash, data)
         });
     }
 

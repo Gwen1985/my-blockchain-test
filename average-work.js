9621d297b1d3252a68e59d0a2623c7a2a9e63f44
gwen1985 <gwen.ellegeest@outlook.com>
2021-02-23 02:59:38 +0100

    testing block logs in average-work.js

--- average-work.js
+++ average-work.js
@@ -5,24 +5,37 @@ const Blockchain = require('./blockchain');
 const blockchain = new Blockchain();
 
 blockchain.addBlock({data: 'initial'});
-
 console.log(`first block`, blockchain.chain[blockchain.chain.length - 1]);
 
-let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
+blockchain.addBlock({data: 'initial'});
+console.log(`first block`, blockchain.chain[blockchain.chain.length - 1]);
 
-const times = [];
+blockchain.addBlock({data: 'initial'});
+console.log(`first block`, blockchain.chain[blockchain.chain.length - 1]);
 
-for (let i = 0; i < 1000; i++) {
-    prevTimestamp = blockchain.chain[blockchain.chain.length - 1].timestamp;
+blockchain.addBlock({data: 'initial'});
+console.log(`first block`, blockchain.chain[blockchain.chain.length - 1]);
 
-    blockchain.addBlock({data: `block ${i}`});
-    nextBlock = blockchain.chain[blockchain.chain.length - 1];
+blockchain.addBlock({data: 'initial'});
+console.log(`first block`, blockchain.chain[blockchain.chain.length - 1]);
 
-    nextTimestamp = nextBlock.timestamp;
-    timeDiff = nextTimestamp - prevTimestamp;
-    times.push(timeDiff);
 
-    average = times.reduce((total, num) => (total + num)) / times.length;
 
-    console.log(chalk.bgGray.green(` Time to mine block: `) + chalk.greenBright(` ${timeDiff}ms. `) + chalk.bgGray.red(`  Difficulty: `) + chalk.redBright(` ${nextBlock.difficulty}. `) + chalk.bgGray.cyan(`  Average time: `) + chalk.cyanBright(` ${average} `));
-}
+// let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
+//
+// const times = [];
+//
+// for (let i = 0; i < 1000; i++) {
+//     prevTimestamp = blockchain.chain[blockchain.chain.length - 1].timestamp;
+//
+//     blockchain.addBlock({data: `block ${i}`});
+//     nextBlock = blockchain.chain[blockchain.chain.length - 1];
+//
+//     nextTimestamp = nextBlock.timestamp;
+//     timeDiff = nextTimestamp - prevTimestamp;
+//     times.push(timeDiff);
+//
+//     average = times.reduce((total, num) => (total + num)) / times.length;
+//
+//     console.log(chalk.bgGray.green(` Time to mine block: `) + chalk.greenBright(` ${timeDiff}ms. `) + chalk.bgGray.red(`  Difficulty: `) + chalk.redBright(` ${nextBlock.difficulty}. `) + chalk.bgGray.cyan(`  Average time: `) + chalk.cyanBright(` ${average} `));
+// }
